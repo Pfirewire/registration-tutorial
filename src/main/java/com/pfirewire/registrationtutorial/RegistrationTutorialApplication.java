07e959745055f63642def00f4f5ae401dedb18b2
Stephen Pflug <stephen@pflug.us>
2022-10-31 08:18:48 -0500

    first commit

---
+++ src/main/java/com/pfirewire/registrationtutorial/RegistrationTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.pfirewire.registrationtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pfirewire/registrationtutorial/RegistrationTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfirewire.registrationtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
